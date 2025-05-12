cfe7519458dbb5ad6b08cf78191f99cd6651fb70
VarshiniMamidala-05 <varshinimamidala13@gmail.com>
2025-05-12 22:04:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Animate fade-in on scroll
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 0.1
+};
+
+const appearOnScroll = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
